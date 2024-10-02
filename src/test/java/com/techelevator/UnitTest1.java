d184116a7ae209e81565efdb4f6d30ad052438d2
Isaac Chow <isaac.chow@techelevator.com>
2024-10-02 00:26:59 -0500

    mod1 capstone initial version

---
+++ src/test/java/com/techelevator/UnitTest1.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class UnitTest1 {
+
+
+	@Test
+	public void testMethod1() {
+	}
+}
